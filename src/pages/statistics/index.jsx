9b6972fd66734798bc521d2265697d6355500255
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-03-01 18:01:40 +0530

    screenshots added

--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -3,13 +3,11 @@ import Wallet from "../../component/wallet";
 import Calender from "../../component/calender";
 import Efficiency from "../../component/revenueFlow/Efficiency";
 import SummaryV2 from "../../component/summary/SummaryV2";
-import LocationV2 from "../../component/location";
 import TaskSummary from "../../component/summary/TaskSummary";
 
 function Statistics() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:flex-1 2xl:mb-0 mb-6">
           <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
@@ -17,7 +15,6 @@ function Statistics() {
             <Efficiency height="h-[180px]" />
           </div>
           <div className="w-full mb-[24px] flex space-x-[24px]">
-            <LocationV2 />
             <TaskSummary />
           </div>
 
