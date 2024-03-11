69ee1caf3952997108e811cd13723a2d3348e953
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-03-11 13:14:52 +0530

    ready to review

--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -9,16 +9,15 @@ import TaskSummary from "../../component/summary/TaskSummary";
 function Statistics() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:flex-1 2xl:mb-0 mb-6">
           <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
             <SummaryV2 />
-            <Efficiency height="h-[180px]" />
+            <TaskSummary />
           </div>
           <div className="w-full mb-[24px] flex space-x-[24px]">
             <LocationV2 />
-            <TaskSummary />
+            <Efficiency height="h-[180px]" />
           </div>
 
           <ListTab />
