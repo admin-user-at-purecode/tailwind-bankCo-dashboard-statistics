2cf32c84b65cf7b19931567e777d8107c9c7d04b
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-03-12 16:58:45 +0530

    ready to review

--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -9,7 +9,6 @@ import TaskSummary from "../../component/summary/TaskSummary";
 function Statistics() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:flex-1 2xl:mb-0 mb-6">
           <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
@@ -18,16 +17,18 @@ function Statistics() {
           </div>
           <div className="w-full mb-[24px] flex space-x-[24px]">
             <LocationV2 />
-            <TaskSummary />
           </div>
+          <TaskSummary />
 
-          <ListTab />
         </section>
         <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
           <Wallet />
           <Calender />
         </section>
       </div>
+      <br />
+      <ListTab />
+
     </main>
   );
 }
