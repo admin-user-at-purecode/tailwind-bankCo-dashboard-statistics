b0c88145d66026c6a2831e64d0fdadc9ac25a8ce
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-03-12 19:37:17 +0530

    functionality remove

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -1,5 +1,4 @@
 import Sidebar from "./sidebar/index";
-import SidebarV2 from "./sidebar/SidebarV2";
 import HeaderOne from "./header/HeaderOne";
 import HeaderTwo from "./header/HeaderTwo";
 import { useState } from "react";
@@ -28,7 +27,6 @@ function Layout({ bg }) {
       >
         <div className="relative flex w-full">
           <Sidebar handleActive={() => setSidebar(!sidebar)} />
-          <SidebarV2 />
           <div
             className={`body-wrapper flex-1 overflow-x-hidden ${
               bg ? bg : "dark:bg-darkblack-500"
--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -9,7 +9,6 @@ import TaskSummary from "../../component/summary/TaskSummary";
 function Statistics() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:flex-1 2xl:mb-0 mb-6">
           <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
